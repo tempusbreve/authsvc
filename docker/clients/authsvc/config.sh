8786c7dbe9c5e70b78af912b398910c15c68df78
John Weldon <johnweldon4@gmail.com>
2018-06-02 14:24:26 -0700

    Update docker script

--- docker/clients/authsvc/config.sh
+++ docker/clients/authsvc/config.sh
@@ -8,6 +8,21 @@ fi
 IMAGE=tempusbreve/authsvc
 NAME="${NAME:-authsvc}"
 PORT=${PORT:-4884}
+BIND_IP=${BIND_IP:-0.0.0.0}
+
+DEBUG=${DEBUG:-}
+CRYPT_HASH=${CRYPT_HASH:-}
+CRYPT_BLOCK=${CRYPT_BLOCK:-}
+CACHE_DIR=${CACHE_DIR:-/data}
+PUBLIC_HOME=${PUBLIC_HOME:-/public}
+STORAGE=${STORAGE:-boltdb}
+
+LDAP_HOST=${LDAP_HOST:-localhost}
+LDAP_PORT=${LDAP_PORT:-389}
+LDAP_TLS=${LDAP_TLS:-false}
+LDAP_ADMIN_USER=${LDAP_ADMIN_USER:-cn=admin,dc=example,dc.com}
+LDAP_ADMIN_PASS=${LDAP_ADMIN_PASS:-password}
+LDAP_BASE_DN=${LDAP_BASE_DN:-dc=example,dc=com}
 
 
 
@@ -24,14 +39,20 @@ function start-container {
   docker run -d \
       --name ${NAME} \
       --restart=always \
-      -e VERBOSE="${VERBOSE:-}" \
+      -e DEBUG="${DEBUG}" \
       -e PORT="${PORT}" \
-      -e SEED_HASH="${SEED_HASH:-}" \
-      -e SEED_BLOCK="${SEED_BLOCK:-}" \
-      -e DATA_HOME="${DATA_HOME:-/data}" \
-      -e PUBLIC_HOME="${PUBLIC_HOME:-/public}" \
-      -e STORAGE="${STORAGE:-boltdb}" \
-      -e CLIENTS="${CLIENTS:-/data/clients.json}" \
+      -e BIND_IP="${BIND_IP}" \
+      -e CRYPT_HASH="${CRYPT_HASH}" \
+      -e CRYPT_BLOCK="${CRYPT_BLOCK}" \
+      -e CACHE_DIR="${CACHE_DIR}" \
+      -e PUBLIC_HOME="${PUBLIC_HOME}" \
+      -e STORAGE="${STORAGE}" \
+      -e LDAP_HOST="${LDAP_HOST}" \
+      -e LDAP_PORT="${LDAP_PORT}" \
+      -e LDAP_TLS="${LDAP_TLS}" \
+      -e LDAP_ADMIN_USER="${LDAP_ADMIN_USER}" \
+      -e LDAP_ADMIN_PASS="${LDAP_ADMIN_PASS}" \
+      -e LDAP_BASE_DN="${LDAP_BASE_DN}" \
       -p ${PORT}:${PORT} \
       -v "${DATA_DIR}":/data \
     ${IMAGE}
